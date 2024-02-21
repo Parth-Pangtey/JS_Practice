9f8a2d803cfb088cc0372f721138535204e8e0b0
Parth Pangtey <Parth.Pangtey@penske.com>
2024-02-21 16:27:00 +0530

    Basic practice

--- test.js
+++ test.js
@@ -1,1 +1,27 @@
-console.log("Parth");
+
+//Array
+
+let num=[1,2,34,2,4,5]
+console.log(num);
+
+//Function
+
+let sum = function(a,b){
+    return a+b;
+}
+console.log(sum(33,2));
+
+//Arrow Function
+let mul = (a,b) => a*b
+console.log(mul(33,2));
+
+
+//Console something
+//console.log("2+2");
+
+//function
+// function sum(a,b){
+//     console.log(a+b);
+// }
+
+// sum(2,4);
